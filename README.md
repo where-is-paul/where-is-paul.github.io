d65a26c8f1c01f17beb3c249c6b9cbbd63f01e13
deep-thought <Paul@deep-thought.(none)>
2013-02-17 20:35:16 -0800

    redesigning website, cleaned up site

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 inutard.github.com
 ==================
+
+A backup of my UBC website as well as a place to experiment with new designs.
--- upload.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python
-import subprocess
-subprocess.call(["git", "add",  "-A", "."])
-subprocess.call(["git", "commit", "-a", \
-                 "--allow-empty-message", "-m", "\'\'"])
-subprocess.call(["git", "push"])
